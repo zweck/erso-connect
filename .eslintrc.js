96f91d40e2aaa9648a619c91dd6b9446ba55d836
Phil Hauser <phil.hauser@disneystreaming.com>
2023-06-26 13:26:03 +0100

    debugging canvas issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,6 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': [0],
     'import/prefer-default-export': [0],
     'jsx-a11y/no-static-element-interactions': [0],
+    'object-curly-spacing': [0],
   },
 };
--- src/the-game/DigitalRain.js
+++ src/the-game/DigitalRain.js
@@ -1,60 +1,58 @@
-import React, { useRef, useEffect } from 'react';
-import { PropTypes } from 'prop-types';
+import React, {useRef, useEffect} from 'react';
+import {PropTypes} from 'prop-types';
 
-const renderMatrix = (ref) => {
-  const canvas = ref.current;
-  const context = canvas.getContext('2d');
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-  const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const nums = '0123456789';
-  const alphabet = katakana + latin + nums;
-
-  const fontSize = 16;
-  const columns = canvas.width / fontSize;
-
-  const rainDrops = [];
-
-  for (let x = 0; x < columns; x += 1) {
-    rainDrops[x] = 1;
-  }
-
-  const render = () => {
-    context.fillStyle = 'rgba(0, 0, 0, 0.05)'; // black w a tiny bit of alpha
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = '#0F0'; // pure green
-    context.font = `${fontSize}px monospace`;
-
-    for (let i = 0; i < rainDrops.length; i += 1) {
-      // randomize the string of characters to render
-      const text = alphabet.charAt(
-        Math.floor(Math.random() * alphabet.length),
-      );
-      context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
-
-      if (
-        rainDrops[i] * fontSize > canvas.height
-        && Math.random() > 0.975
-      ) {
-        rainDrops[i] = 0;
-      }
-      rainDrops[i] += 1;
-    }
-  };
-
-  return () => {
-    setInterval(render, 50);
-  };
-};
-
-function MatrixRainingLetters({ customClass = '' }) {
+function MatrixRainingLetters({customClass = ''}) {
   const ref = useRef();
   const thisClassName = `mrl-container ${customClass}`;
-  useEffect(() => renderMatrix(ref));
+  useEffect(() => {
+    const canvas = ref.current;
+    const context = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+    const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const nums = '0123456789';
+    const alphabet = katakana + latin + nums;
+
+    const fontSize = 16;
+    const columns = canvas.width / fontSize;
+
+    const rainDrops = [];
+
+    for (let x = 0; x < columns; x += 1) {
+      rainDrops[x] = 1;
+    }
+
+    const render = () => {
+      context.fillStyle = 'rgba(0, 0, 0, 0.05)'; // black w a tiny bit of alpha
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      context.fillStyle = '#0F0'; // pure green
+      context.font = `${fontSize}px monospace`;
+
+      for (let i = 0; i < rainDrops.length; i += 1) {
+        // randomize the string of characters to render
+        const text = alphabet.charAt(
+          Math.floor(Math.random() * alphabet.length),
+        );
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (
+          rainDrops[i] * fontSize > canvas.height
+          && Math.random() > 0.975
+        ) {
+          rainDrops[i] = 0;
+        }
+        rainDrops[i] += 1;
+      }
+    };
+
+    return () => {
+      setInterval(render, 50);
+    };
+  });
 
   return (
     <canvas className={thisClassName} ref={ref} />
